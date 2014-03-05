de40a8e9cca7872890a7250accd8896faee06f3e
Paul Bourke <pauldbourke@gmail.com>
2014-03-05 20:38:54 +0000

    8.1.1 Add sessions controller
    Add new controller to handle sessions, along with some routes, view and
    initial test.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ TodoApp::Application.routes.draw do
   root 'static_pages#home'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  get "users/new"
   get "static_pages/home"
   get "static_pages/about"
+  get "users/new"
 
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 end
